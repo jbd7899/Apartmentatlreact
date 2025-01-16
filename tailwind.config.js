b2514a928eae0d34084a47e31f8dd6606026c948
jbd7899 <jbd7899@me.com>
2025-01-15 19:25:41 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#D2691E',
+        'primary-dark': '#8B4513',
+        secondary: '#6C757D',
+        text: '#3E3E3E',
+        'text-light': '#777',
+        background: '#FAF3E0',
+        highlight: '#F9F1E7'
+      }
+    },
+  },
+  plugins: [],
+}
